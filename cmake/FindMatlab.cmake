1017ec5c2198d99afd9faa02ece81c4ad65a6b26
haefner <haefner@in.tum.de>
2018-05-05 14:22:11 +0200

    start including mac support

--- cmake/FindMatlab.cmake
+++ cmake/FindMatlab.cmake
@@ -7,11 +7,10 @@
 
 SET(MATLAB_FOUND 0)
 IF( "$ENV{MATLAB_ROOT}" STREQUAL "" )
-    MESSAGE(STATUS "MATLAB_ROOT environment variable not set." )
-    MESSAGE(STATUS "In Linux this can be done in your user .bashrc file by appending the corresponding line, e.g:" )
-    MESSAGE(STATUS "export MATLAB_ROOT=/usr/local/MATLAB/R2016b" )
-    MESSAGE(STATUS "In Windows this can be done by adding system variable, e.g:" )
-    MESSAGE(STATUS "MATLAB_ROOT=D:\\Program Files\\MATLAB\\R2011a" )
+    MESSAGE(SEND_ERROR "MATLAB_ROOT environment variable not set or found, please do:  
+                        export MATLAB_ROOT=/usr/local/MATLAB/R2016b in ~/.bashrc (Linux)  
+                        export MATLAB_ROOT=/Applications/MATLAB_R2016b.app in ~/.bash_profile (MacOS)  
+                        Add system variable MATLAB_ROOT=D:\\Program Files\\MATLAB\\R2016b (Windows) (not yet tested)" )
 ELSE("$ENV{MATLAB_ROOT}" STREQUAL "" )
 
         FIND_PATH(MATLAB_INCLUDE_DIR mex.h
@@ -22,12 +21,18 @@ ELSE("$ENV{MATLAB_ROOT}" STREQUAL "" )
         FIND_LIBRARY( MATLAB_MEX_LIBRARY
                       NAMES libmex mex
                       PATHS $ENV{MATLAB_ROOT}/bin $ENV{MATLAB_ROOT}/extern/lib 
-                      PATH_SUFFIXES glnxa64 glnx86 win64/microsoft win32/microsoft)
+                      PATH_SUFFIXES
+                      glnxa64 glnx86 #linux
+                      win64/microsoft win32/microsoft #windows
+                      maci maci64) #macos
 
         FIND_LIBRARY( MATLAB_MX_LIBRARY
                       NAMES libmx mx
                       PATHS $ENV{MATLAB_ROOT}/bin $ENV{MATLAB_ROOT}/extern/lib 
-                      PATH_SUFFIXES glnxa64 glnx86 win64/microsoft win32/microsoft)
+                      PATH_SUFFIXES
+                      glnxa64 glnx86 #linux
+                      win64/microsoft win32/microsoft #windows
+                      maci maci64) #macos
 
     MESSAGE (STATUS "MATLAB_ROOT: $ENV{MATLAB_ROOT}")
 
